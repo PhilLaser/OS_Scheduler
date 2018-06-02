09d315e7c5691f966573f495e8bf1009a60c2fb1
Temp <temp@Temps-MacBook.local>
2018-06-03 01:01:03 +0200

    what

--- out/production/lab2_solution/appJS/lab2_solution.js
+++
@@ -1,8 +0,0 @@
-let thread = new Threaddy("meme", 12, 3, "(5,4), (5,4), (10,12)");
-
-console.log(thread.name);
-console.log(thread.bursts);
-console.log(thread.IOBurstList);
-console.log(thread.CPUBurstList);
-console.log(thread.priority);
-console.log(thread.arrivalTime);
